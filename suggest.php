b23bc67d8dc2aac1fd44725a97603253f7fa5bf1
Mauricio Eduardo <olive.y.ra@hotmail.com>
2016-07-19 19:30:07 -0300

    navegacao de tela e carregamento de dados

---
+++ suggest.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = "Sugestão de Media";
+$section = "suggest";
+include("inc/header.php"); ?>
+
+<div class="section page">
+    <h1>Sugestão de Media</h1>
+</div>
+
+<?php include("inc/footer.php"); ?>
